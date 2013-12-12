85a64f0f050b0ac659b3b0d9a5b048ec57f4e3e0
Nina <ninamarie315@gmail.com>
2013-12-12 18:58:19 -0500

    Fixing up how events are show aka adding attending a guest events. Also set up stub host logic.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,6 +6,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @events = Event.all
+    @user = current_user
   end
 
   # GET /events/1
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def except_current_user
+    current_user.blank? ? User.all : User.find(:all, :conditions => ["id != ?", current_user.id])
+  end
 end
