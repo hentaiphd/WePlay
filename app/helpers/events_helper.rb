23f14a746cf4e34c1d4075f29eedfbf982919c7d
Nina <ninamarie315@gmail.com>
2013-12-12 20:20:50 -0500

    When making an event users can only bring their own games

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,9 @@ module EventsHelper
   def except_current_user
     current_user.blank? ? User.all : User.find(:all, :conditions => ["id != ?", current_user.id])
   end
+
+  def users_games
+    @user = current_user
+    return @user.games
+  end
 end
